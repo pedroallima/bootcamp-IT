de302b1dbf540487cfc70793d4c1404a1a1773fa
pedroallima <pedro.leao@mercadolivre.com>
2021-08-22 16:22:09 -0300

    pastas e jar

--- exerciciojava1/src/Exercicio8.java
+++ exerciciojava1/src/Exercicio8.java
@@ -2,8 +2,10 @@ import java.util.Random;
 public class Exercicio8 {
     public static void main(String[] args) {
         Random random = new Random();
-        int[][] cliente = new int[50][2];
+        int[][] cliente = new int[50][3];
         int jornalA = 0;
+        int jornalB = 0;
+        int jornalC = 0;
         int jornalABC = 0;
         int jornalAC = 0;
         int jornalAB = 0;
@@ -11,36 +13,40 @@ public class Exercicio8 {
         int outos = 0;
 
         for (int i = 0; i < 50; i++) {
-            for (int k = 0; k < 2; k++) {
-                int numerok = random.nextInt(2);
+            for (int k = 0; k <= 2; k++) {
+                int numerok = random.nextInt(3);
                 cliente[i][k] = numerok;
-                System.out.println(""+numerok);
             }
 
-            switch (cliente[i]['0']){
+
+            int vetor0 = cliente[i][0];
+            int vetor1 = cliente[i][1];
+            int vetor2 = cliente[i][2];
+
+
+            switch (vetor0){
                 case 0: // primeiro A
-                    switch (cliente[i]['1']){
+                    switch (vetor1){
                         case 0:
-                            switch (cliente[i]['2']){
+                            switch (vetor2){
                                 case 0:
                                     jornalA = jornalA+1; // A A A
                                     break;
                                 case 1:
-                                    jornalAB = jornalAB+1;// A A B
+                                    jornalAB = jornalAB+1; // A A B
                                     break;
                                 case 2:
                                     jornalAC =jornalAC+1; // A A C
                                     break;
                             }
-
                             break;
                         case 1: // Segundo B
-                            switch (cliente[i]['2']){
+                            switch (vetor2){
                                 case 0: // terceiro A
                                     jornalAB = jornalAB+1; // A B A
                                     break;
                                 case 1: // terceiro B
-                                    jornalAB = jornalAB+1;// A B B
+                                    jornalAB = jornalAB+1; // A B B
                                     break;
                                 case 2: // Terceiro C
                                     jornalABC =jornalABC+1; // A B C
@@ -49,7 +55,7 @@ public class Exercicio8 {
 
                             break;
                         case 2: // Segundo C
-                            switch (cliente[i]['2']){
+                            switch (vetor2){
                                 case 0: // Terceiro A
                                     jornalAC = jornalAC+1; // A C A
                                     break;
@@ -65,9 +71,9 @@ public class Exercicio8 {
 
                     break;
                 case 1: //primeiro B
-                    switch (cliente[i]['1']){
+                    switch (vetor1){
                         case 0: // segundo A
-                            switch (cliente[i]['2']){
+                            switch (vetor2){
                                 case 0: // terceiro A
                                     jornalAB = jornalAB+1; // B A A
                                     break;
@@ -81,12 +87,12 @@ public class Exercicio8 {
 
                             break;
                         case 1:// segundo B
-                            switch (cliente[i]['2']){
+                            switch (vetor2){
                                 case 0: // terceiro A
                                     jornalAB = jornalAB+1; // B B A
                                     break;
                                 case 1: // terceiro B
-                                    outos = outos+1;// B B B
+                                    jornalB = jornalB+1;// B B B
                                     break;
                                 case 2: // terceiro C
                                     jornalBC =jornalBC+1; // B B C
@@ -95,7 +101,7 @@ public class Exercicio8 {
 
                             break;
                         case 2: // segundo C
-                            switch (cliente[i]['2']){
+                            switch (vetor2){
                                 case 0: // terceiro A
                                     jornalABC = jornalABC+1; // B C A
                                     break;
@@ -111,9 +117,9 @@ public class Exercicio8 {
 
                     break;
                 case 2: // primeiro C
-                    switch (cliente[i]['1']){
+                    switch (vetor1){
                         case 0: // segundo A
-                            switch (cliente[i]['2']){
+                            switch (vetor2){
                                 case 0: // terceiro A
                                     jornalAC = jornalAC+1; // C A A
                                     break;
@@ -127,7 +133,7 @@ public class Exercicio8 {
 
                             break;
                         case 1: // segundo B
-                            switch (cliente[i]['2']){
+                            switch (vetor2){
                                 case 0: // terceiro A
                                     jornalABC = jornalABC+1; // C B A
                                     break;
@@ -141,7 +147,7 @@ public class Exercicio8 {
 
                             break;
                         case 2: // segundo C
-                            switch (cliente[i]['2']){
+                            switch (vetor2){
                                 case 0: // terceiro A
                                     jornalAC = jornalAC+1; // C C A
                                     break;
@@ -149,7 +155,7 @@ public class Exercicio8 {
                                     jornalBC = jornalBC+1;// C C B
                                     break;
                                 case 2: // terceiro C
-                                    outos =outos+1; // C C C
+                                    jornalC =jornalC+1; // C C C
                                     break;
                             }
                             break;
@@ -162,14 +168,13 @@ public class Exercicio8 {
 
         }
 
-        System.out.println("Sao assinate dos jornais A, B e C "+jornalABC);
-        System.out.println("Sao assinate dos jornais A e C "+jornalAC);
-        System.out.println("Sao assinate dos jornais A e B "+jornalAB);
-        System.out.println("Sao assinate dos jornais B e C "+jornalBC);
-        System.out.println("Sao assinate dos jornais A "+jornalA);
-        System.out.println("Sao assinate dos jornais Outros "+outos);
-
-
+        System.out.println("Sao assinate dos jornais A, B e C =" + jornalABC);
+        System.out.println("Sao assinate dos jornais A e C    =" + jornalAC);
+        System.out.println("Sao assinate dos jornais A e B    =" + jornalAB);
+        System.out.println("Sao assinate dos jornais B e C    =" + jornalBC);
+        System.out.println("Sao assinate dos jornais A        =" + jornalA);
+        System.out.println("Sao assinate dos jornais B        =" + jornalB);
+        System.out.println("Sao assinate dos jornais C        =" + jornalC);
 
 
     }
