30040a98b421b3c70c29e1304e71fa93d6f967de
pedroallima <pedro.leao@mercadolivre.com>
2021-08-28 14:09:01 -0300

    teste

--- exercicio-java/modulo5/java-poo-3/Pratrica-integradora-2/src/com/meli/Corrida.java
+++ exercicio-java/modulo5/java-poo-3/Pratrica-integradora-2/src/com/meli/Corrida.java
@@ -1,73 +1,60 @@
 package com.meli;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Corrida {
-/*----------------------------FEITO--------------------------------------
-     2. Modelar a classe Corrida que tem os seguintes ATRIBUTOS:
-        ● Distancia ● PremioEmDolares ● Nome ● QuantidadeDeVeiculosPermitidos ● ListaDeVeiculos
----------------------------------------------------------------------------*/
+
     float Distancia;
     float PremioEmDolares;
     String Nome;
     int QuantidadeDeVeiculosPermitidos;
-    private SocorristaCarro socorristaCarro = new SocorristaCarro(100,20,90,"2424");
+    // para utilizar soccorista
+    private SocorristaCarro socorristaCarro;
+    private SocorristaMoto socorristaMoto;
+    private List<Veiculo> corredores;
 
-    private SocorristaMoto socorristaMoto = new SocorristaMoto(100,20,90,"2424");
-   List<Veiculo> corredores = new ArrayList<>();
-
+    // criacao da corrida ok
     public Corrida(float distancia, float premioEmDolares, String nome, int quantidadeDeVeiculosPermitidos) {
-        Distancia = distancia;
-        PremioEmDolares = premioEmDolares;
-        Nome = nome;
-        QuantidadeDeVeiculosPermitidos = quantidadeDeVeiculosPermitidos;
-    }
+        this.Distancia = distancia;
+        this.PremioEmDolares = premioEmDolares;
+        this.Nome = nome;
+        this.QuantidadeDeVeiculosPermitidos = quantidadeDeVeiculosPermitidos;
+        // iniciando as classes e collections aqui no metodo construtor
+        this.socorristaCarro = new SocorristaCarro();
+        this.socorristaMoto = new SocorristaMoto();
+        this.corredores  = new ArrayList<>();
 
-    /*--------------------------------------CCORRIDA----------------------------------------------
-        Uma corrida também tem um conjunto de veículos que participam da corrida.
-        Portanto, agora a corrida terá a responsabilidade de poder adicionar um veículo à corrida, por isso devemos definir os seguintes métodos:
-        -> public void adicionarCarro(velocidade, aceleracao, AnguloDeVirada, placa);
-        -> public void adicionarMoto(velocidade, aceleracao, AngulaDeVirada, placa);
-        Ambos os métodos acrescentam um veículo, desde que haja espaço.
-        --------------------------------------------------------------------------------------------*/
+    }
 
     public boolean isListaCheia() {
         if (corredores.size() == QuantidadeDeVeiculosPermitidos) {
-            System.out.println("Limite maximo de inscritos!");
             return true;
         }
         return false;
     }
 
-
-
    public void adicionarCarro(Veiculo carro){
        // quantidade menor que o valor da corrida
        if (isListaCheia()) {
+           System.out.println("Limite maximo de inscritos!");
            return;
        }
           corredores.add(carro);
-       System.out.println("Carro Adicionadas na corrida!");
+          System.out.println("Carro Adicionadas na corrida!");
 
    }
 
     public void adicionarMoto(Veiculo moto){
         // quantidade menor que o valor da corrida
         if (isListaCheia()) {
+            System.out.println("Limite maximo de inscritos!");
             return;
         }
-        corredores.add(moto);
-        System.out.println("Carro Adicionadas na corrida!");
+            corredores.add(moto);
+            System.out.println("Carro Adicionadas na corrida!");
 
     }
-
-    /* ----------- 4 ------------------
-    Também teremos a possibilidade de remover um veículo por meio de dois métodos:
-    -> public void removerVeiculo(vehículo);
-    -> public void removeVeiculoComPlaca(String umaPlaca);
-    --------------------------------------- */
 
     public void removerVeiculo(String veiculo) {
         for (int i = 0; i < corredores.size(); i++){
@@ -88,14 +75,7 @@ public class Corrida {
     }
 
 
-    /* ------------------- 6 ---------------------------------------
-    Queremos ser capazes de definir o vencedor de uma corrida:
-    O vencedor será aquele que tiver o valor máximo determinado pela seguinte fórmula:
-    Velocidade * ½ Aceleração / (AnguloDeVirada*(Peso - NumeroDeRodas * 100).
-    -----------------------------------------------------------------*/
-
     public double calculaVencedor() {
-
         double valorMaximo = 0.0;
             for (int i = 0; i < corredores.size(); i++) {
             double resultado = corredores.get(i).velocidade * (corredores.get(i).aceleracao) + (corredores.get(i).anguloDeVirada) * (corredores.get(i).anguloDeVirada * (corredores.get(i).peso - corredores.get(i).rodas * 100));
@@ -105,36 +85,38 @@ public class Corrida {
             }
         }
         return valorMaximo;
-
     }
 
 
-    /*
-    ----------------------- 7 ---------------------------------------------------
-    As corridas também têm veículos de emergência que vão e consertam um veículo
-    em caso de emergência. Como os carros são muito diferentes das motos, existem dois
-    tipos diferentes de veículos de resgate. Um deles só sabe como resgatar carros e o
-    outro só sabe como resgatar motos.
-    ------------------------------------------------------------------------------*/
-
-
-    /*
-            a) Acrescente um socorrista de carro e um socorrista de moto à classe de corrida. (OK)
-            b) Acrescente à corrida a responsabilidade de resgatar uma moto e um carro:
-        public void socorrerCarro(String placa);
-        public void socorrerMoto(String placa);
-    */
     public void socorrerCarro(String placa) {
-            socorristaCarro.socorrer(new Carro(placa));
-            removeVeiculoComPlaca(placa);
-            System.out.println(corredores.size());
+        for (int i = 0; i < corredores.size(); i++) {
+            if(corredores.get(i).placa.equals(placa)&&(corredores instanceof Carro)){
+                socorristaCarro.socorrer((Carro) corredores);
+                return;
+            }
         }
+        System.out.println("NENHUM CARRO COM ESTA PLACA NA CORRIDA!!!!");
+    }
 
     public void socorrerMoto(String placa) {
-        socorristaMoto.socorrer(new Moto(placa));
-        removeVeiculoComPlaca(placa);
-        System.out.println(corredores.size());
+        for (int i = 0; i < corredores.size(); i++) {
+            if(corredores.get(i).placa.equals(placa)&&(corredores instanceof Moto)){
+                socorristaMoto.socorrer((Carro) corredores);
+                return;
+            }
+        }
+        System.out.println("NENHUM CARRO COM ESTA PLACA NA CORRIDA!!!!");
     }
+
+
+    public void mostrarVeiculosDaCorrida(){
+        System.out.println("*******VEÍCULOS INSCRITOS***********");
+        for (int i = 0; i < corredores.size(); i++) {
+            System.out.println(corredores.get(i).placa);
+        }
+    }
+
+
 
 
 }
--- exercicio-java/modulo5/java-poo-3/Pratrica-integradora-2/src/com/meli/Veiculo.java
+++ exercicio-java/modulo5/java-poo-3/Pratrica-integradora-2/src/com/meli/Veiculo.java
@@ -1,5 +1,7 @@
 package com.meli;
 
+import java.util.List;
+import java.util.ArrayList;
 public class Veiculo {
     protected String tipo;
     public String marca;
@@ -7,10 +9,12 @@ public class Veiculo {
     public double velocidade;
     public double aceleracao;
     public double anguloDeVirada;
-    protected int rodas;
-    protected int peso;
+    public int rodas;
+    public double peso;
 
-    public Veiculo(String marca, String placa, double velocidade, double aceleracao, double anguloDeVirada) {
+
+
+    public Veiculo(String marca, String placa, double velocidade, double aceleracao, double anguloDeVirada, double peso, int rodas) {
         this.marca = marca;
         this.placa = placa;
         this.velocidade = velocidade;
@@ -18,30 +22,22 @@ public class Veiculo {
         this.anguloDeVirada = anguloDeVirada;
     }
 
-    public Veiculo(double velocidade, double aceleracao, double anguloDeVirada, String placa){
-        this.placa = placa;
-        this.velocidade = velocidade;
-        this.aceleracao = aceleracao;
-        this.anguloDeVirada = anguloDeVirada;
+    public String getPlaca() {
+        return placa;
     }
-
-    public Veiculo(String placa) {
-        this.placa = placa;
-    }
-    /*
-    Uma corrida também tem um conjunto de veículos que participam da corrida.
-    Portanto, agora a corrida terá a responsabilidade de poder adicionar um veículo à corrida, por isso devemos definir os seguintes métodos:
-    -> public void adicionarCarro(velocidade, aceleracao, AnguloDeVirada, placa);
-    -> public void adicionarMoto(velocidade, aceleracao, AngulaDeVirada, placa);
-    Ambos os métodos acrescentam um veículo, desde que haja espaço.
-    */
-
 
     @Override
     public String toString() {
-        return  "\n"+tipo+" + tipo  + "+  marca +" + marca  + "+ placa+ " + placa  "+ velocidade + " velocidade "+
-                ", aceleracao=" + aceleracao + ", anguloDeVirada=" + anguloDeVirada + ", rodas=" + rodas + ", peso=" + peso;
+        return "Veiculo{" +
+                "placa='" + placa + '\'' +
+                ", velocidade=" + velocidade +
+                ", aceleracao=" + aceleracao +
+                ", anguloDeVirada=" + anguloDeVirada +
+                ", peso=" + peso +
+                ", quantRodas=" + rodas +
+                '}';
     }
+
 
 }
 
